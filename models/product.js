cceaa323a247a22afb856bc885fc16b33bd30325
afonso <afonsoinfo@gmail.com>
2016-02-13 14:21:06 -0200

    improves product model

--- models/product.js
+++ models/product.js
@@ -2,7 +2,22 @@ var Product = function(data){
   var product;
   data = data || {};
   product = {
-    name: data.nome || ""
+    id: data.id,
+    skuId: data.sku || "",
+    name: data.nome || "",
+    slug: data.apelido || "",
+    description: data.descricao_completa || "",
+    resource_uri: data.resource_uri,
+    brand: "",
+    categories: [],
+    images: [],
+    weight: "",
+    height: "",
+    width: "",
+    depth: "",
+    listPrice: "",
+    price: "",
+    costPrice: ""
   };
   return product
 };
